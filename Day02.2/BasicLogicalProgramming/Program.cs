599189f0f02463a6668e7442c999bd696b4f41cc
ravi04ez <ezekie.em@gmail.com>
2022-07-06 07:50:49 +0530

    Welcome message to Logical Programming

---
+++ Day02.2/BasicLogicalProgramming/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasicLogicalProgramming
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t##################################################");
+            Console.WriteLine("\t\t\t\t\tWELCOME TO BASIC LOGICAL PROGRAMMING");
+            Console.WriteLine("\t\t\t\t##################################################");
+            Console.ReadKey();
+
+
+        }
+    }
+}
